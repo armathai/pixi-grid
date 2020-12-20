2d4a946afe697e330ce106132030d2e42625842c
saqsun <sargsyan.sargis.89@gmail.com>
2020-12-20 13:51:57 +0400

    rollup fix

--- rollup.config.js
+++ rollup.config.js
@@ -3,78 +3,60 @@ import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import pkg from './package.json';
 
-const extensions = ['.js', '.ts'];
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
-export default [
-    // browser-friendly UMD build
-    {
-        input: 'src/index.ts',
-        output: {
-            name: 'pixi-grid',
-            file: pkg.browser,
-            format: 'umd',
+const name = 'pixi-grid';
+
+export default {
+    input: './src/index.ts',
+
+    // Specify here external modules which you don't want to include in your bundle (for instance: 'lodash', 'moment' etc.)
+    // https://rollupjs.org/guide/en#external-e-external
+    external: [],
+
+    plugins: [
+        // Allows node_modules resolution
+        resolve({ extensions }),
+
+        // Allow bundling cjs modules. Rollup doesn't understand cjs
+        commonjs(),
+
+        // Compile TypeScript/JavaScript files
+        babel({
+            extensions,
+            include: ['src/**/*'],
+            presets: [
+                // [
+                //     '@babel/preset-env',
+                //     {
+                //         useBuiltIns: 'usage',
+                //         corejs: { version: 3, proposals: true },
+                //         bugfixes: true,
+                //         debug: true,
+                //     },
+                // ],
+                '@babel/preset-typescript',
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties'],
+        }),
+    ],
+
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs',
         },
-        plugins: [
-            resolve({ extensions }), // so Rollup can find `ms`
-            commonjs(), // so Rollup can convert `ms` to an ES module
-            babel({
-                extensions,
-                exclude: ['node_modules/**'],
-                presets: [
-                    [
-                        '@babel/preset-env',
-                        {
-                            useBuiltIns: 'usage',
-                            modules: false,
-                            corejs: { version: 3, proposals: true },
-                            targets: {
-                                browsers: ['ios >= 10', 'and_chr >= 20', 'safari >= 8'],
-                            },
-                            debug: true,
-                        },
-                    ],
-                    '@babel/preset-typescript',
-                ],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-            }),
-        ],
-    },
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it's quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify
-    // `file` and `format` for each target)
-    {
-        input: 'src/index.ts',
-        external: [],
-        output: [
-            { file: pkg.main, format: 'cjs' },
-            { file: pkg.module, format: 'es' },
-        ],
-        plugins: [
-            resolve({ extensions }),
-            babel({
-                extensions,
-                exclude: ['node_modules/**'],
-                presets: [
-                    [
-                        '@babel/preset-env',
-                        {
-                            useBuiltIns: 'usage',
-                            // modules: false,
-                            corejs: { version: 3, proposals: true },
-                            targets: {
-                                browsers: ['ios >= 10', 'and_chr >= 20', 'safari >= 8'],
-                            },
-                            debug: true,
-                        },
-                    ],
-                    '@babel/preset-typescript',
-                ],
-                plugins: ['@babel/plugin-proposal-class-properties'],
-            }),
-        ],
-    },
-];
+        {
+            file: pkg.module,
+            format: 'es',
+        },
+        {
+            file: pkg.browser,
+            format: 'iife',
+            name,
+            extend: true,
+            // https://rollupjs.org/guide/en#output-globals-g-globals
+            globals: {},
+        },
+    ],
+};
